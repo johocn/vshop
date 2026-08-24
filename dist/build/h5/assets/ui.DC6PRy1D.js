929eee87179366e8999d0c84b74e7a05924a01b7
johocn <johocn@163.com>
2026-08-24 22:03:28 +0800

    build(web-admin): 角色页 onShow 刷新 + 默认角色一键导入 dist 产物

--- dist/build/h5/assets/ui.DC6PRy1D.js
+++
@@ -1,1 +0,0 @@
-import{at as a,k as o,ad as n,af as t,R as s}from"./index-BHswJ_ni.js";const i=a("ui",()=>{const a=o(!1),i=o("");return{loading:a,toastMessage:i,showLoading:function(){a.value=!0,n({title:"加载中..."})},hideLoading:function(){a.value=!1,t()},showToast:function(a,o="none"){s({title:a,icon:o,duration:2e3})}}});export{i as u};
