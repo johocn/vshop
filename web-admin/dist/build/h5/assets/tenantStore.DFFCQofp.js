2129355557b357b4d297fc603f572192b9ee5c42
johocn <johocn@163.com>
2026-09-20 07:12:49 +0800

    build(h5): 重新构建含 F3/F4 与批1 手册挂图的 H5 产物

---
+++ web-admin/dist/build/h5/assets/tenantStore.DFFCQofp.js
@@ -0,0 +1,1 @@
+import{a0 as e,a4 as t,T as o,a5 as s}from"./index--IF7YkZN.js";const a=e("tenant",{state:()=>({code:s(),token:o(),name:""}),actions:{selectCh(e,o){this.code=e.code,this.token=e.token,this.name=o??e.code,t(e.code,e.token)},clear(){this.code="",this.token="",this.name=""}}});export{a as u};
