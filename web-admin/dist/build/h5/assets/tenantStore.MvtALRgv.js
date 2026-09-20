f601dc078ca453b3cfc8cc6f4c2ea193095af87c
johocn <johocn@163.com>
2026-09-20 19:28:47 +0800

    build(h5): 业务域i18n迁移构建产物

--- web-admin/dist/build/h5/assets/tenantStore.MvtALRgv.js
+++
@@ -1,1 +0,0 @@
-import{a1 as e,a5 as t,U as o,a6 as s}from"./index-g9_dRsGd.js";const a=e("tenant",{state:()=>({code:s(),token:o(),name:""}),actions:{selectCh(e,o){this.code=e.code,this.token=e.token,this.name=o??e.code,t(e.code,e.token)},clear(){this.code="",this.token="",this.name=""}}});export{a as u};
