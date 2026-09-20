7b5096f8d7aa92a40d9e35ab6dfbf306e2e00a8e
johocn <johocn@163.com>
2026-09-20 20:41:38 +0800

    build(h5): 语言切换按钮构建产物

---
+++ web-admin/dist/build/h5/assets/tenantStore.BYXU2Tes.js
@@ -0,0 +1,1 @@
+import{a1 as e,a5 as t,U as o,a6 as s}from"./index-COqJiEQ_.js";const a=e("tenant",{state:()=>({code:s(),token:o(),name:""}),actions:{selectCh(e,o){this.code=e.code,this.token=e.token,this.name=o??e.code,t(e.code,e.token)},clear(){this.code="",this.token="",this.name=""}}});export{a as u};
